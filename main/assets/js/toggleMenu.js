29c87810df6406d9a56dcfaab179be96feeae28a
Mitchell Gould <mitchellgould7@gmail.com>
2021-02-11 14:47:46 -0500

    add js to control the menu

--- main/assets/js/toggleMenu.js
+++ main/assets/js/toggleMenu.js
@@ -1,18 +1,8 @@
-const hamburger = document.getElementById("hamburger");
-const cross = document.getElementById("cross");
-const header__nav = document.querySelector(".header__nav");
-console.log(header__nav);
-
-hamburger.addEventListener('click', () => {
-  toggleClasses();
-})
-
-cross.addEventListener('click', () => {
-  toggleClasses();
-})
+const menuIcons = document.querySelector(".menu-icons")
+menuIcons.addEventListener('click', () => toggleClasses())
 
 const toggleClasses = () => {
-  header__nav.classList.toggle("open");
-  hamburger.classList.toggle("is-hidden");
-  cross.classList.toggle("is-hidden");
+  document.querySelector(".header__nav").classList.toggle("open");
+  document.getElementById("hamburger").classList.toggle("is-hidden");
+  document.getElementById("cross").classList.toggle("is-hidden");
 }
